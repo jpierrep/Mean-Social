943a080d9426d672aa3e6b48d87fff085a5382c9
jpierrep <jpena.suazo@gmail.com>
2018-08-09 22:29:54 -0400

    guardando usuarios, cifrando contraseña, con error http

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,5 @@
 'use strict'
+var bcrypt=require('bcrypt-nodejs');
 
 var User= require('../models/user');
 
@@ -18,8 +19,62 @@ function home (req,res){
         })
         console.log(req.body);
          };
+
+         //registrar nuevos usuarios
+         function saveUser(req,res){
+            var params=req.body;
+            var user=new User();
+            
+
+            if(params.name&&params.surname&&params.email&&params.password){
+                user.name=params.name;
+                user.surname=params.surname;
+                user.nick=params.nick;
+                user.email=params.email;
+                user.role='ROLE_USER';
+                user.image=null;
+
+                //verificar que el usuario no existe
+                //$or: y un array en
+              //  User.findOne({email:user.email.toLowerCase()});
+                
+              //comprobar usuarios duplicados
+              User.find({$or:[
+                    {email:user.email.toLowerCase()},
+                    {nick:user.nick.toLowerCase()}
+                    
+                ]}).exec((err,users)=>{
+                    if(err) return res.status(500).send({message:'Error en la peticion de usuarios'});
+                   //console.log(users +'tamaño'+users.length);
+                    if(users && users.length>=1)  return res.status(200).send({message:'El usuario que intenta registrar ya existe'});  
+                
+                });
+
+                //trae opciones null como el costo del cifrado,iterar el hash para fortificar la contraseña
+                //por defecto viene con un coste de 10
+                
+                bcrypt.hash(params.password,null,null,(err,hash)=>{
+                    user.password=hash;
+                    user.save((err,userStored)=>{
+                        if(err) return res.status(500).send({message:'Error al guardar el usuario'});
+                        //si se creo el usuario 
+                        if(userStored){
+                            res.status(200).send({user:userStored});  
+                        }else{
+                            res.status(404).send({message:'No se ha registrado el usuario'});  
+                        }
+                    }); 
+                });
+
+            }else{
+                //los datos son necesarios
+                res.status(200).send({message:'Envia todos los campos necesario'});
+            }
+            
+
+         }
     
          
          module.exports={
-             home,pruebas
+             home,pruebas,saveUser
          }
