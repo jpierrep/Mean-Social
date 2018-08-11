bf9ccb6785e4df0d70c2e248060e8e239984b78b
Jean Pierre <jpena.suazo@gmail.com>
2018-08-11 18:25:29 -0400

    añade follows

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -12,7 +12,26 @@ res.status(200).send({message:'Hola mundo desde el controller follows'})
 
 }
 
+function saveFollow(req,res){
+    
+    
+    var params=req.body;
+    var follow=new Follow();
+    //tengo el usuario identificado desde que creo el token
+    follow.user=req.user.sub; //solo consideramos el id
+    console.log(params.followed);
+     params.followed
+    follow.followed=params.followed;
+    follow.save((err,followStored)=>{
+        if(err) return res.status(500).send({message:'Error al guardar el seguimiento'});
+        if(!followStored) return res.status(404).send({message:'Error el seguimiento no se ha guardado'});
+
+        return res.status(200).send({follow:followStored});
+    });
+
+}
+
 
 module.exports={
-    prueba
+    prueba,saveFollow
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -6,8 +6,12 @@ var api=express.Router();
 var md_auth=require('../middelwares/authenticated');
 
 api.get('/pruebas-follow',md_auth.ensureAuth,FollowController.prueba);
+//hacer la request por x-www-for-encoded para post
+api.post('/follow',md_auth.ensureAuth,FollowController.saveFollow);
 
 module.exports=api;
+
+
 
 
 
