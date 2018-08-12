92760e5ccfffdeb40ee899394ca26fee9d1493dc
jpierrep <jpena.suazo@gmail.com>
2018-08-12 03:29:05 -0300

    añade contador de followings

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -31,7 +31,50 @@ function saveFollow(req,res){
 
 }
 
+function deleteFollow(req,res){
+     var userId=req.user.sub;
+     var followId=req.params.id
+    // Follow.findByIdAndRemove
+    Follow.find({'user':userId,'followed':followId}).remove(err=>{
+     if(err) return    res.status(500).send({message:'Error al dejar de seguir'});
+      return  res.status(200).send({message:'El follow se ha eliminado'})
+
+    
+    });
+}
+
+    function getFollowingUsers(req,res){
+        var userId=req.user.sub;
+        //dar flexibilidad con el parametro opcional
+        if(req.params.id && req.params.page){
+            userId=req.params.id;
+        }
+        var page=1;
+        if(req.params.page){
+            page=req.params.page;
+        }else{
+            //por si no viene el usuario sino la pagina primero 
+            //ej: http://localhost:3800/api/following/2
+            page=req.params.id;
+        }
+        var itemsPerPage=4;
+        //nos entrega el objeto populado al que hace referencia el campo followed que es un id de usuario
+        Follow.find({user:userId}).populate({path:'followed'}).paginate(page,itemsPerPage,(err,follows,total)=>{
+            if(err) return    res.status(500).send({message:'Error en el servidor'});
+            if(!follows) return res.status(500).send({message:'No estas siguiendo a ningun usuario'});
+            return res.status(200).send({
+                total:total,
+                pages:Math.ceil(total/itemsPerPage),
+                follows
+            });
+        });
+    }
+
+
+
+
+
 
 module.exports={
-    prueba,saveFollow
+    prueba,saveFollow,deleteFollow,getFollowingUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -8,6 +8,11 @@ var md_auth=require('../middelwares/authenticated');
 api.get('/pruebas-follow',md_auth.ensureAuth,FollowController.prueba);
 //hacer la request por x-www-for-encoded para post
 api.post('/follow',md_auth.ensureAuth,FollowController.saveFollow);
+//http delete nos permite eliminar recursos del backend
+//http://localhost:3800/api/follow/5b6cf6912bc7cd745030e927 añadiendo el token y el metodo delete
+api.delete('/follow/:id',md_auth.ensureAuth,FollowController.deleteFollow);
+//http://localhost:3800/api/following/5b6fbb532fa8408b3ca1c1d8/1 pasando el token Authorization en el header
+api.get('/following/:id?/:page?',md_auth.ensureAuth,FollowController.getFollowingUsers);
 
 module.exports=api;
 
